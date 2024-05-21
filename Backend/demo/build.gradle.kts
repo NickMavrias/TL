3a7bb9509edabdc4d19ce94b8db8b93427c6621b
NickTsioutsias <117465959+NickTsioutsias@users.noreply.github.com>
2024-05-21 10:41:06 +0300

    WebSocket configuration added

--- Backend/demo/build.gradle.kts
+++ Backend/demo/build.gradle.kts
@@ -23,6 +23,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-websocket")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.mapstruct:mapstruct:1.4.2.Final")
