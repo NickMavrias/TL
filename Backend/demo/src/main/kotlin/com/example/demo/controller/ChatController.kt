0ad5d59da18092d7f01afdbc085d665829c161b3
NickTsioutsias <117465959+NickTsioutsias@users.noreply.github.com>
2024-05-21 14:40:51 +0300

    Basic chatting feature

---
+++ Backend/demo/src/main/kotlin/com/example/demo/controller/ChatController.kt
@@ -0,0 +1,28 @@
+package com.example.demo.controller
+
+import com.example.demo.dto.ChatMessageDto
+import com.example.demo.service.ChatMessageService
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.SendTo
+import org.springframework.messaging.handler.annotation.Payload
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor
+import org.springframework.stereotype.Controller
+
+@Controller
+class ChatController(private val chatMessageService: ChatMessageService) {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    fun sendMessage(@Payload chatMessage: ChatMessageDto): ChatMessageDto {
+        chatMessageService.saveMessage(chatMessage)
+        return chatMessage
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    fun addUser(@Payload chatMessage: ChatMessageDto, headerAccessor: SimpMessageHeaderAccessor): ChatMessageDto {
+        headerAccessor.sessionAttributes?.put("username", chatMessage.sender)
+        chatMessageService.saveMessage(chatMessage)
+        return chatMessage
+    }
+}
