c41f32c8e9390fc2a64d84b8492dbef5889283a8
NickMavrias <up1084664@upnet.gr>
2024-05-24 20:00:31 +0300

    implemented alt routes

--- Code/eureka/lib/android_large14.dart
+++ Code/eureka/lib/android_large14.dart
@@ -1,10 +1,9 @@
-//epafes
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:your_app_name/android_large13.dart';
 import 'package:your_app_name/android_large15.dart';
-
 import 'android_large16.dart';
+import 'androidlarge11.dart';
 import 'androidlarge11.dart';
 
 class AndroidLarge14 extends StatefulWidget {
@@ -18,51 +17,50 @@ class _AndroidLarge14State extends State<AndroidLarge14> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        title: Padding(
-          padding: EdgeInsets.only(top: 40),
-          child: SvgPicture.asset(
-            'assets/images/logo_telikoCOLOURED-02.svg',
-            height: 100,
-            width: 150,
-          ),
-        ),
-        centerTitle: true,
-      ),
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: 'Αναζήτηση φιλ@',
-                suffixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: BorderSide(color: Color(0xFFE4572E), width: 3),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 40),  // Space from the top
+            Center(
+              child: SvgPicture.asset(
+                'assets/images/logo_telikoCOLOURED-02.svg',
+                height: 100,
+                width: 150,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Αναζήτηση φιλ@',
+                  suffixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(color: Color(0xFFE4572E), width: 3),
+                  ),
                 ),
               ),
             ),
-          ),
-          UserTile(
-            name: 'Νίκος',
-            imagePath: 'assets/images/user1.png',
-            dateRead: 'Διαβάσατε στις 1/5/505',
-          ),
-          UserTile(
-            name: 'Γιώργος',
-            imagePath: 'assets/images/user2.png',
-            dateRead: 'Διαβάσατε στις 29/8/2019',
-          ),
-          UserTile(
-            name: 'Γιώργος',
-            imagePath: 'assets/images/user3.png',
-            dateRead: 'Διαβάσατε στις 31/10/2013',
-          ),
-          
-        ],
+            UserTile(
+              name: 'Νίκος',
+              imagePath: 'assets/images/user1.png',
+              dateRead: 'Διαβάσατε στις 1/5/505',
+              index: 1,
+            ),
+            UserTile(
+              name: 'Γιώργος',
+              imagePath: 'assets/images/user2.png',
+              dateRead: 'Διαβάσατε στις 29/8/2019',
+              index: 2,
+            ),
+            UserTile(
+              name: 'Γιώργος',
+              imagePath: 'assets/images/user3.png',
+              dateRead: 'Διαβάσατε στις 31/10/2013',
+              index: 3,
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
@@ -70,29 +68,25 @@ class _AndroidLarge14State extends State<AndroidLarge14> {
           setState(() {
             _currentIndex = index;
             if (index == 0){
-            Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => AndroidLarge11()),
-            );
-          }
-          else if (index == 1){
-            Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => AndroidLarge13()),
-            );
-          }
-          else if (index == 2) {
-            Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => AndroidLarge14()),
-            );
-          }
-          else if (index == 3){
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => AndroidLarge11()),
+              );
+            } else if (index == 1){
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => AndroidLarge13()),
+              );
+            } else if (index == 2) {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => AndroidLarge14()),
+              );
+            } else if (index == 3){
               Navigator.of(context).push(
                 MaterialPageRoute(builder: (context) => AndroidLarge15()));  // Navigate to AndroidLarge15
+            } else if (index == 4){
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => AndroidLarge16()),
+              );
             }
-            else if (index == 4){
-            Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => AndroidLarge16()),
-            );
-          }
           });
         },
         items: [
@@ -126,13 +120,21 @@ class UserTile extends StatelessWidget {
   final String name;
   final String imagePath;
   final String dateRead;
+  final int index;
 
   const UserTile({
     Key? key,
     required this.name,
     required this.imagePath,
     required this.dateRead,
+    required this.index,
   }) : super(key: key);
+
+  void handleAction(String value) {
+    // Example of how to handle actions. Implement according to your needs.
+    print('$value User $index');
+    // Here you would handle each action.
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -143,7 +145,27 @@ class UserTile extends StatelessWidget {
       ),
       title: Text(name, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
       subtitle: Text(dateRead, style: TextStyle(color: Colors.grey)),
-      trailing: Icon(Icons.message, color: Color(0xFFE4572E)),
+      trailing: PopupMenuButton<String>(
+        onSelected: handleAction,
+        itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+          const PopupMenuItem<String>(
+            value: 'Unmatch',
+            child: Text('Unmatch'),
+          ),
+          const PopupMenuItem<String>(
+            value: 'Report',
+            child: Text('Report'),
+          ),
+          const PopupMenuItem<String>(
+            value: 'Unfriend',
+            child: Text('Unfriend'),
+          ),
+          const PopupMenuItem<String>(
+            value: 'Block',
+            child: Text('Block'),
+          ),
+        ],
+      ),
       onTap: () {
         // Implement navigation to chat or detail screen
       },
